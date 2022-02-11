34b270fcc9e56782bc62c13c4fab9424a6e903d9
pradiknatama <pradiknatama@gmail.com>
2022-02-11 09:11:49 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.index');
 });
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+// Route::get('/login', function () {
+//     return view('login');
+// })->name('login');
 Route::get('/checkout', function () {
     return view('pages.checkout');
 })->name('pages.checkout');
 Route::get('/success', function () {
     return view('pages.success_checkout');
 })->name('success');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
